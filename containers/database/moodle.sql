10b2d2f06c3c87cd0ed8612eb2a7006e571e37e2
Steve Shilling <stevshil@users.noreply.github.com>
2021-10-15 11:11:56 +0100

    No more moodle

--- containers/database/moodle.sql
+++ containers/database/moodle.sql
@@ -6,3 +6,9 @@ GRANT ALL PRIVILEGES ON *.* TO 'moodle'@'%';
 GRANT ALL PRIVILEGES ON *.* TO 'moodle'@'localhost';
 flush privileges;
 -- GRANT SELECT ON performance_schema.* TO 'exporter'@'dbmon';
+-- Set moodle attributes
+SET GLOBAL innodb_compression_default=ON;
+SET GLOBAL innodb_file_per_table=ON;
+SET GLOBAL innodb_file_format='Barracuda';
+SET GLOBAL innodb_default_row_format='dynamic';
+SET SESSION  innodb_compression_default=ON;
